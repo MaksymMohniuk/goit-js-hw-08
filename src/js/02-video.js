e8645c7645542dad024d45973efe0a42d1175369
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-01-31 00:29:31 +0200

    finish-task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    const onTimeUpdate = function(data) {
+        localStorage.setItem('videoplayer-current-time', data.seconds);
+        console.log(data.seconds);
+            }
+
+    player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+
+    player.setCurrentTime(savedTime || 0);
+    
+    
+    
+    
+    
+   
+
+
+    
