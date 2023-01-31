942fa0c1ea0e15a268be4ff2ca25a50c3bf0fa8e
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-01-31 23:28:07 +0200

    start-task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+let formData = {};
+
+form.addEventListener('input', throttle(getUserData, 500));
+form.addEventListener('submit', handleSubmit);
+
+function getUserData(event) {
+localStorage.setItem('feedback-form-state', JSON.stringify(event.target.value));
+}
