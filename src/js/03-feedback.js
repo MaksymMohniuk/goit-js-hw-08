cf076bffd553f5929a5715c390384d81b559589d
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-02-01 23:23:38 +0200

    finish-task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,5 +9,27 @@ form.addEventListener('input', throttle(getUserData, 500));
 form.addEventListener('submit', handleSubmit);
 
 function getUserData(event) {
-localStorage.setItem('feedback-form-state', JSON.stringify(event.target.value));
+formData[event.target.name] = event.target.value;
+localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+reloadPage();
+
+function reloadPage() {
+    if(localStorage.getItem('feedback-form-state')) {
+        input.value = JSON.parse(localStorage.getItem('feedback-form-state'))?.email;
+        textarea.value = JSON.parse(localStorage.getItem('feedback-form-state'))?.message;
+    } else {
+        input.value = ' ';
+        textarea.value = ' ';
+    }
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    console.log(savedData);
+
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
 }
