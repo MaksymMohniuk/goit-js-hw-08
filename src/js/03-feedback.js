2d1d0db40b43a9eaec66014edda72d9f1c59b82a
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-02-04 00:21:05 +0200

    task-3-last-correction

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,39 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('.feedback-form input');
 const textarea = document.querySelector('.feedback-form textarea');
 let formData = {};
 const savedValues = localStorage.getItem('feedback-form-state');
-console.log(savedValues);
 const savedDataObject = JSON.parse(savedValues);
 
 form.addEventListener('input', throttle(getUserData, 500));
 form.addEventListener('submit', handleSubmit);
 
 function getUserData(event) {
-formData[event.target.name] = event.target.value;
-localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      ...formData,
+      [event.target.name]: event.target.value,
+    })
+  );
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-    console.log(savedData);
-
-    event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
-    formData = {};
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  formData = {};
 }
 
 function reloadPage() {
-    if(localStorage.getItem('feedback-form-state')) {
-        formData = localStorage.getItem('feedback-form-state');
-        input.value = savedDataObject.email || '';
-        textarea.value = savedDataObject.message || '';
-    } 
+  if (localStorage.getItem('feedback-form-state')) {
+    formData.email = savedDataObject?.email;
+    formData.message = savedDataObject?.message;
+    input.value = savedDataObject?.email || '';
+    textarea.value = savedDataObject?.message || '';
+  }
 }
 reloadPage();
